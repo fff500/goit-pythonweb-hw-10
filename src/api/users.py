4de23183195a9ab1ba2ebf9365ff4020d1cd6c86
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-06-20 01:28:57 +0100

    users/me requests qty was restricted

--- src/api/users.py
+++ src/api/users.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 from src.schemas import UserModel
 from src.services.auth import get_current_user
 
 
 router = APIRouter(prefix="/users", tags=["users"])
+limiter = Limiter(key_func=get_remote_address)
 
 
-@router.get("/me", response_model=UserModel)
-async def me(user: UserModel = Depends(get_current_user)):
+@router.get(
+    "/me", response_model=UserModel, description="No more than 2 requests per minute"
+)
+@limiter.limit("2/minute")
+async def me(request: Request, user: UserModel = Depends(get_current_user)):
     return user
